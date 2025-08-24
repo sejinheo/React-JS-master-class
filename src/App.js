73bf5032fa374345ba74cfc17eab7e0310f4816d
허세진 <heosejin222@gmail.com>
2025-08-25 01:32:05 +0900

    [STYLED COMPONENTS] 'AS' and Attrs

--- src/App.js
+++ src/App.js
@@ -4,22 +4,20 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  height: 100px;
-  width: 100px;
+const Input = styled.input.attrs({required:true})`
+  background-color: tomato;
 `;
 
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
 
 function App() {
-  return ( 
-  <Father>
-   <Box bgColor="teal"/>
-   <Circle bgColor="tomato"/>
-  </Father>
+  return (
+    <Father as="header">
+     <Input />
+     <Input />
+     <Input />
+     <Input />
+     <Input />
+    </Father>
   );
 }
 
