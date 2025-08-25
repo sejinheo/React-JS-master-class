e13371a0d17bc85fb7c524bb59cd8be73c75a8f4
허세진 <heosejin222@gmail.com>
2025-08-26 01:06:44 +0900

    [CRYPTO TRACKER] Setup

---
+++ src/routes/Coin.tsx
@@ -0,0 +1,13 @@
+import { ExtractRouteParams } from "react-router";
+import { useParams } from "react-router-dom";
+
+interface RouteParams {
+    coinId: string;
+}
+
+function Coin() {
+    const {coinId} = useParams<RouteParams>();
+    return <h1>Coin: {coinId}</h1>
+}
+
+export default Coin;
